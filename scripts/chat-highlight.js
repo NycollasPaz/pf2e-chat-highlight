b16f8dfcfd126c661435948c186eaebe15bef696
NycollasPaz <nycollaspaz@gmail.com>
2026-08-06 11:39:40 -0300

    Implement link highlighting in chat messages
    Add chat message rendering hook to highlight links

--- scripts/chat-highlight.js
+++ scripts/chat-highlight.js
@@ -1,1 +1,20 @@
+Hooks.on("renderChatMessage", (message, html) => {
 
+    html.find("a.content-link").each(function () {
+
+        const link = $(this);
+
+        const uuid = link.attr("data-uuid");
+
+        if (!uuid) return;
+
+        if (
+            uuid.includes("Item") ||
+            uuid.includes("Condition")
+        ) {
+            link.addClass("pf2e-chat-highlight");
+        }
+
+    });
+
+});
